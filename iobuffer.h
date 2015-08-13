a0cee623425522e2aacd317c744835e48a05796e
ltlollo <ltlollo.it@gmail.com>
2015-08-13 10:22:29 +0200

    naming and use std

--- iobuffer.h
+++ iobuffer.h
@@ -1,30 +1,30 @@
 #ifndef IOBUFFER_H
 #define IOBUFFER_H
 
-template<typename T, size_t N>
+template<typename T, std::size_t N>
 struct Conf {
-    using type = T;
-    static constexpr size_t size = N;
+    using Type = T;
+    static constexpr std::size_t size = N;
 };
 
 template<typename T>
 struct Err {
     T err;
-    size_t pos;
+    std::size_t pos;
 };
 
 template<typename Cfg, typename Cn, typename Ds, typename Ret>
 struct IOBuffer {
-    static constexpr size_t size = Cfg::size;
-    using type = typename Cfg::type;
-    type storage[size]{};
+    static constexpr std::size_t size = Cfg::size;
+    using Ele = typename Cfg::Type;
+    Ele storage[size]{};
     Cn cnstr;
     Ds destr;
     Ret err;
     constexpr IOBuffer(Cn c, Ds d) noexcept : cnstr{c}, destr{d} {
     }
-    auto group_cnstr(const size_t pos = 0) noexcept {
-        for (size_t i = pos; i < size; ++i) {
+    auto group_cnstr(const std::size_t pos = 0) noexcept {
+        for (std::size_t i = pos; i < size; ++i) {
             if ((err = cnstr(storage[i]))) {
                 return Err<Ret>{ err, i };
             }
@@ -32,7 +32,7 @@ struct IOBuffer {
         return Err<Ret>{ {}, size };
     }
     auto group_destr() noexcept {
-        for (size_t i = 0; i < size; ++i) {
+        for (std::size_t i = 0; i < size; ++i) {
             destr(storage[i]);
         }
     }
@@ -47,20 +47,20 @@ struct IOBuffer {
 
 template<typename Cfg, typename Cn, typename Ds>
 struct IOBuffer<Cfg, Cn, Ds, void> {
-    static constexpr size_t size = Cfg::size;
-    using type = typename Cfg::type;
-    type storage[size]{};
+    static constexpr std::size_t size = Cfg::size;
+    using Ele = typename Cfg::Type;
+    Ele storage[size]{};
     Cn cnstr;
     Ds destr;
     constexpr IOBuffer(Cn c, Ds d) noexcept : cnstr{c}, destr{d} {
     }
-    auto group_cnstr(const size_t pos = 0) noexcept {
-        for (size_t i = pos; i < size; ++i) {
+    auto group_cnstr(const std::size_t pos = 0) noexcept {
+        for (std::size_t i = pos; i < size; ++i) {
             cnstr(storage[i]);
         }
     }
     auto group_destr() noexcept {
-        for (size_t i = 0; i < size; ++i) {
+        for (std::size_t i = 0; i < size; ++i) {
             destr(storage[i]);
         }
     }
@@ -75,15 +75,15 @@ struct IOBuffer<Cfg, Cn, Ds, void> {
 
 template<typename Cfg, typename Cn, typename Ret>
 struct IOBuffer<Cfg, Cn, void, Ret> {
-    static constexpr size_t size = Cfg::size;
-    using type = typename Cfg::type;
-    type storage[size];
+    static constexpr std::size_t size = Cfg::size;
+    using Ele = typename Cfg::Type;
+    Ele storage[size];
     Cn cnstr;
     Ret err;
     constexpr IOBuffer(Cn c) noexcept : cnstr{c} {
     }
-    auto group_cnstr(const size_t pos = 0) noexcept {
-        for (size_t i = pos; i < size; ++i) {
+    auto group_cnstr(const std::size_t pos = 0) noexcept {
+        for (std::size_t i = pos; i < size; ++i) {
             if ((err = cnstr(storage[i]))) {
                 return Err<Ret>{ err, i };
             }
@@ -100,14 +100,14 @@ struct IOBuffer<Cfg, Cn, void, Ret> {
 
 template<typename Cfg, typename Cn>
 struct IOBuffer<Cfg, Cn, void, void> {
-    static constexpr size_t size = Cfg::size;
-    using type = typename Cfg::type;
-    type storage[size];
+    static constexpr std::size_t size = Cfg::size;
+    using Ele = typename Cfg::Type;
+    Ele storage[size];
     Cn cnstr;
     constexpr IOBuffer(Cn c) noexcept : cnstr{c} {
     }
-    auto group_cnstr(const size_t pos = 0) noexcept {
-        for (size_t i = pos; i < size; ++i) {
+    auto group_cnstr(const std::size_t pos = 0) noexcept {
+        for (std::size_t i = pos; i < size; ++i) {
             cnstr(storage[i]);
         }
     }
@@ -121,13 +121,13 @@ struct IOBuffer<Cfg, Cn, void, void> {
 
 template<typename C, typename F, typename G>
 auto make_iobuffer(C, F f, G g) noexcept {
-    using Ret = decltype(f(declval<typename C::type&>()));
+    using Ret = decltype(f(std::declval<typename C::Type&>()));
     return IOBuffer<C, F, G, Ret>{f, g};
 }
 
 template<typename C, typename F>
 auto make_iobuffer(C, F f) noexcept {
-    using Ret = decltype(f(declval<typename C::type&>()));
+    using Ret = decltype(f(std::declval<typename C::Type&>()));
     return IOBuffer<C, F, void, Ret>{f};
 }
 
